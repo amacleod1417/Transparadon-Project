61a90225d54b6bd02cdaba993a24f222d679eb2f
onkr0d <90716666+onkr0d@users.noreply.github.com>
2024-05-31 03:26:46 -0500

    start on contributors

--- contracts/transparadon/src/lib.rs
+++ contracts/transparadon/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
+use soroban_sdk::{contract, contractimpl, symbol_short, vec, Address, Env, Symbol, Vec};
 
 #[contract]
 pub struct TransparadonContract;
@@ -8,6 +8,16 @@ pub struct TransparadonContract;
 impl TransparadonContract {
     pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
         vec![&env, symbol_short!("Hello"), to]
+    }
+
+    // Contribute to the fund, and store the address of the user who contributed
+    pub fn addContributor(env: Env, address: Address) {
+        // save the address of the user who deposited in a vec
+        const key: &str = "depositors";
+        let dep: Vec<Address> = env.storage().instance().get(&key).unwrap_or(vec![&env]);
+        let newDepositors:Vec<Address> = vec![&env, address];
+        newDepositors.append(&newDepositors);
+        env.storage().instance().set(&key, &dep);
     }
 }
 
