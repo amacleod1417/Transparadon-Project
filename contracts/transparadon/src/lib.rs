303fb937dc809de14c4c4405b99f6666943e2f08
Vickie Luo <87625331+luovickie@users.noreply.github.com>
2024-05-31 07:22:33 -0400

    Add files via upload

--- contracts/transparadon/src/lib.rs
+++ contracts/transparadon/src/lib.rs
@@ -1,29 +1,194 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, symbol_short, vec, Address, Env, Symbol, Vec};
+use soroban_sdk::{contract, contractimpl, contracttype, vec, Address, BytesN, Env, IntoVal, Vec};
 
-#[contract]
-pub struct TransparadonContract;
-
-#[contractimpl]
-impl TransparadonContract {
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        vec![&env, symbol_short!("Hello"), to]
-    }
-
-    // Contribute to the fund, and store the address of the user who contributed
-    pub fn add_contributor(env: Env, contributor: Address) {
-        const key: &str = "contributors";
-        let mut contributors = env
-                    .storage()
-                    .instance()
-                    .get(&key)
-                    .unwrap_or(vec![&env, contributor.clone()]);
-                contributors.push_back(contributor);
-                env.storage()
-                    .instance()
-                    .set(&key, &contributors);
-    }
-
+#[derive(Clone)]
+#[contracttype]
+pub enum StorageKey {
+    Contribution(Address),
+    ContributorList,
+    DepositToken,
+    RewardToken,
+    CampaignActive,
+    ContractAdmin,
+    ContractInitialized,
 }
 
+#[contract]
+pub struct CharityContract;
+
+#[contractimpl]
+impl CharityContract {
+    /// Initialize the contract with the admin address and the deposit token contract address.
+    /// Deploys the reward token contract and initializes it.
+    ///
+    /// # Arguments
+    /// - `env` - The execution environment of the contract.
+    /// - `admin` - The address of the admin.
+    /// - `token_wasm_hash` - The hash of the token contract wasm.
+    /// - `token` - The address of the deposit token contract.
+    pub fn initialize(env: Env, admin: Address, token_wasm_hash: BytesN<32>, deposit_token: Address) {
+        env.storage().instance().set(&StorageKey::ContractAdmin, &admin);
+
+        let reward_contract = token::create_contract(&env, token_wasm_hash, &deposit_token);
+
+        token::Client::new(&env, &reward_contract).initialize(
+            &env.current_contract_address(),
+            &18u32,
+            &"Reward Token".into_val(&env),
+            &"REWARD".into_val(&env),
+        );
+
+        env.storage().instance().set(&StorageKey::DepositToken, &deposit_token);
+        env.storage().instance().set(&StorageKey::RewardToken, &reward_contract);
+        env.storage().instance().set(&StorageKey::ContractInitialized, &true);
+    }
+
+    /// Get the status of the campaign
+    pub fn check_campaign_status(env: Env) -> bool {
+        env.storage()
+            .instance()
+            .get(&StorageKey::CampaignActive)
+            .unwrap_or(false)
+    }
+
+    /// Records a deposit made by a contributor if the campaign is active.
+    ///
+    /// # Arguments
+    /// - `env` - The execution environment of the contract.
+    /// - `contributor` - The address of the contributor making the contribution.
+    /// - `deposit_token` - The address of the token to deposit.
+    /// - `amount` - The amount of contribution in tokens.
+    pub fn contribute(env: Env, contributor: Address, deposit_token: Address, amount: i128) {
+        contributor.require_auth();
+
+        let campaign_active: bool = Self::check_campaign_status(env.clone());
+        if !campaign_active {
+            panic!("The campaign is inactive");
+        }
+
+        token::Client::new(&env, &deposit_token).transfer(
+            &contributor,
+            &env.current_contract_address(),
+            &amount,
+        );
+
+        let reward_token = Self::get_reward_token(env.clone());
+        token::Client::new(&env, &reward_token).mint(&contributor, &amount);
+
+        Self::set_contribution(env.clone(), contributor.clone(), amount);
+    }
+
+    /// Withdraws the contribution made by a contributor if the campaign is active.
+    ///
+    /// # Arguments
+    /// - `env` - The execution environment of the contract.
+    /// - `contributor` - The address of the contributor making the contribution.
+    /// - `recipient` - The address of the recipient of the contribution.
+    /// - `deposit_token` - The address of the token to withdraw.
+    pub fn withdraw(env: Env, contributor: Address, recipient: Address, deposit_token: Address) {
+        contributor.require_auth();
+
+        let campaign_active = Self::check_campaign_status(env.clone());
+        if !campaign_active {
+            panic!("The campaign is inactive");
+        }
+
+        let contribution_amount = Self::get_contribution(env.clone(), contributor.clone());
+        token::Client::new(&env, &deposit_token).transfer(
+            &env.current_contract_address(),
+            &recipient,
+            &contribution_amount,
+        );
+
+        let reward_token = Self::get_reward_token(env.clone());
+        token::Client::new(&env, &reward_token).burn(&contributor, &contribution_amount);
+    }
+
+    /// Clear the contributor from the storage
+    pub fn clear_contributor(env: Env, contributor: Address) {
+        env.storage().instance().remove(&StorageKey::Contribution(contributor));
+    }
+
+    /// Get a user's total contribution
+    pub fn get_contribution(env: Env, contributor: Address) -> i128 {
+        env.storage()
+            .instance()
+            .get(&StorageKey::Contribution(contributor))
+            .unwrap_or(0)
+    }
+
+    /// Set a user's contribution
+    pub fn set_contribution(env: Env, contributor: Address, amount: i128) {
+        env.storage()
+            .instance()
+            .set(&StorageKey::Contribution(contributor), &amount);
+    }
+
+    /// Get the list of contributors
+    pub fn get_contributors(env: Env) -> Vec<Address> {
+        env.storage()
+            .instance()
+            .get(&StorageKey::ContributorList)
+            .unwrap_or(vec![&env, env.current_contract_address()])
+    }
+
+    /// Get the total contributions
+    pub fn get_total_contributions(env: Env) -> i128 {
+        let contributors = Self::get_contributors(env.clone());
+        let mut total = 0;
+        for contributor in contributors.iter() {
+            total += Self::get_contribution(env.clone(), contributor.clone());
+        }
+        total
+    }
+
+    /// Get the RewardToken address
+    pub fn get_reward_token(env: Env) -> Address {
+        env.storage()
+            .instance()
+            .get(&StorageKey::RewardToken)
+            .unwrap_or(env.current_contract_address())
+    }
+
+    /// Get user's reward token balance
+    pub fn get_reward_token_balance(env: Env, user: Address) -> i128 {
+        let reward_token = Self::get_reward_token(env.clone());
+        token::Client::new(&env, &reward_token).balance(&user)
+    }
+
+    /// Check if a user is a contributor
+    fn is_contributor(env: Env, contributor: Address) -> bool {
+        env.storage()
+            .instance()
+            .get(&StorageKey::Contribution(contributor))
+            .unwrap_or(false)
+    }
+
+    /// Add a new admin
+    pub fn add_new_admin(env: Env, new_admin: Address) {
+        Self::update_admin(env, new_admin);
+    }
+
+    /// Sets the new admin address in the storage.
+    fn update_admin(env: Env, new_admin: Address) {
+        let current_admin = env
+            .storage()
+            .instance()
+            .get(&StorageKey::ContractAdmin)
+            .unwrap_or(env.current_contract_address());
+
+        current_admin.require_auth();
+        env.storage().instance().set(&StorageKey::ContractAdmin, &new_admin);
+    }
+
+    /// Get the admin address
+    pub fn get_admin(env: Env) -> Address {
+        env.storage()
+            .instance()
+            .get(&StorageKey::ContractAdmin)
+            .unwrap_or(env.current_contract_address())
+    }
+}
+
+mod token;
 mod test;
--- contracts/transparadon/src/test.rs
+++ contracts/transparadon/src/test.rs
@@ -1,17 +1,158 @@
 #![cfg(test)]
+extern crate std;
 
-use super::*;
-use soroban_sdk::{symbol_short, vec, Env};
+use super::{token, CharityContract, CharityContractClient};
+use soroban_sdk::{testutils::Address as _, Address, BytesN, Env};
+
+fn create_token_contract<'a>(e: &Env, admin: &Address) -> token::Client<'a> {
+    token::Client::new(e, &e.register_stellar_asset_contract(admin.clone()))
+}
+
+fn install_token_wasm(env: &Env) -> BytesN<32> {
+    soroban_sdk::contractimport!(file = "./token/soroban_token_contract.wasm");
+    env.deployer().upload_contract_wasm(WASM)
+}
 
 #[test]
-fn test() {
+fn initialize() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, TransparadonContract);
-    let client = TransparadonContractClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let token_wasm_hash = install_token_wasm(&env);
+    let deposit_token = create_token_contract(&env, &admin);
 
-    let words = client.hello(&symbol_short!("Dev"));
-    assert_eq!(
-        words,
-        vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
-    );
+    let contract_id = env.register_contract(None, CharityContract);
+    let client = CharityContractClient::new(&env, &contract_id);
+
+    client.initialize(&admin, &token_wasm_hash, &deposit_token.address);
+
+    assert_eq!(client.get_admin(), admin);
+    assert_eq!(client.get_reward_token(), deposit_token.address);
+    assert!(client.check_campaign_status());
+}
+
+#[test]
+fn contribute() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let contributor = Address::generate(&env);
+    let token_wasm_hash = install_token_wasm(&env);
+    let deposit_token = create_token_contract(&env, &admin);
+
+    let contract_id = env.register_contract(None, CharityContract);
+    let client = CharityContractClient::new(&env, &contract_id);
+
+    deposit_token.mint(&contributor, &1000);
+    assert_eq!(deposit_token.balance(&contributor), 1000);
+
+    client.initialize(&admin, &token_wasm_hash, &deposit_token.address);
+    client.contribute(&contributor, &deposit_token.address, &100);
+
+    assert_eq!(client.get_contribution(&contributor), 100);
+    assert_eq!(client.get_total_contributions(), 100);
+}
+
+#[test]
+fn withdraw() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let contributor = Address::generate(&env);
+    let recipient = Address::generate(&env);
+    let token_wasm_hash = install_token_wasm(&env);
+    let deposit_token = create_token_contract(&env, &admin);
+
+    let contract_id = env.register_contract(None, CharityContract);
+    let client = CharityContractClient::new(&env, &contract_id);
+
+    deposit_token.mint(&contributor, &1000);
+    assert_eq!(deposit_token.balance(&contributor), 1000);
+
+    client.initialize(&admin, &token_wasm_hash, &deposit_token.address);
+    client.contribute(&contributor, &deposit_token.address, &100);
+    client.withdraw(&contributor, &recipient, &deposit_token.address);
+
+    assert_eq!(client.get_contribution(&contributor), 0);
+    assert_eq!(client.get_total_contributions(), 0);
+}
+
+#[test]
+fn add_new_admin() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let new_admin = Address::generate(&env);
+    let token_wasm_hash = install_token_wasm(&env);
+    let deposit_token = create_token_contract(&env, &admin);
+
+    let contract_id = env.register_contract(None, CharityContract);
+    let client = CharityContractClient::new(&env, &contract_id);
+
+    client.initialize(&admin, &token_wasm_hash, &deposit_token.address);
+    client.add_new_admin(&new_admin);
+
+    assert_eq!(client.get_admin(), new_admin);
+}
+
+#[test]
+fn clear_contributor() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let contributor = Address::generate(&env);
+    let token_wasm_hash = install_token_wasm(&env);
+    let deposit_token = create_token_contract(&env, &admin);
+
+    let contract_id = env.register_contract(None, CharityContract);
+    let client = CharityContractClient::new(&env, &contract_id);
+
+    client.initialize(&admin, &token_wasm_hash, &deposit_token.address);
+    client.contribute(&contributor, &deposit_token.address, &100);
+    client.clear_contributor(&contributor);
+
+    assert_eq!(client.get_contribution(&contributor), 0);
+}
+
+#[test]
+fn get_contributors() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let contributor1 = Address::generate(&env);
+    let contributor2 = Address::generate(&env);
+    let token_wasm_hash = install_token_wasm(&env);
+    let deposit_token = create_token_contract(&env, &admin);
+
+    let contract_id = env.register_contract(None, CharityContract);
+    let client = CharityContractClient::new(&env, &contract_id);
+
+    client.initialize(&admin, &token_wasm_hash, &deposit_token.address);
+    client.contribute(&contributor1, &deposit_token.address, &100);
+    client.contribute(&contributor2, &deposit_token.address, &200);
+
+    let contributors = client.get_contributors();
+    assert_eq!(contributors.len(), 2);
+    assert!(contributors.contains(&contributor1));
+    assert!(contributors.contains(&contributor2));
+}
+
+#[test]
+fn get_total_contributions() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let contributor1 = Address::generate(&env);
+    let contributor2 = Address::generate(&env);
+    let token_wasm_hash = install_token_wasm(&env);
+    let deposit_token = create_token_contract(&env, &admin);
+
+    let contract_id = env.register_contract(None, CharityContract);
+    let client = CharityContractClient::new(&env, &contract_id);
+
+    client.initialize(&admin, &token_wasm_hash, &deposit_token.address);
+    client.contribute(&contributor1, &deposit_token.address, &100);
+    client.contribute(&contributor2, &deposit_token.address, &200);
+
+    assert_eq!(client.get_total_contributions(), 300);
 }
