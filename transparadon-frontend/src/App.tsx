13113091adade48e80120c4ae13ee590faaf2c88
Noah Kim <itsnoahisnotalright@gmail.com>
2024-07-20 20:11:13 -0700

    continued updating home page

--- transparadon-frontend/src/App.tsx
+++ transparadon-frontend/src/App.tsx
@@ -1,18 +1,13 @@
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div className = 'wrapper'>
       <NavBar/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
---
+++ transparadon-frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className = 'footer-wrapper'>
+        <div className = 'contact'>
+            <h4>
+                Contact Us!
+            </h4>
+            <b>
+                danwlung@gmail.com
+            </b>
+            <b>
+                eatmydick@pp.com
+            </b>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
